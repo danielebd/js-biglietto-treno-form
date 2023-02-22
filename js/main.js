f0735dd021cab376210bafe969690712ccbed2e6
danielebd <danielebalebalestro@gmail.com>
2023-02-22 18:40:08 +0100

    calcolo prezzo finale con stampa console

--- js/main.js
+++ js/main.js
@@ -6,12 +6,30 @@
 inputButton.addEventListener('click',
     function () {
         const percorso = document.getElementById("km").value;
-        document.getElementById("km").innerHTML = percorso;
         console.log('km da percorrere: ' + percorso);
 
-        const eta = document.getElementById("age").value;
-        document.getElementById('age').innerHTML = eta;
+        const eta = document.getElementById("age").options.selectedIndex;
         console.log('età passeggero: ' + eta);
+
+        let prezzo = parseFloat(percorso * 0.21);
+        console.log(`${prezzo} €`);
+
+        if(eta == 2) {
+            prezzo -= prezzo * 0.4;
+            //prezzo = prezzo - prezzo * 0.4;
+            console.log('prezzo scontato over 65 è di:' + prezzo + '€');
+        }
+        
+        else if(eta == 1) {
+            prezzo -= prezzo * 0.2;
+            //prezzo = prezzo  - prezzo * 0.2;
+            console.log('prezzo scontato per i minori 18 è di: ' + prezzo + '€');
+        }
+        
+        
+        prezzo = prezzo.toFixed(2);
+        console.log(prezzo + '€');
+        
     }
 )
 
