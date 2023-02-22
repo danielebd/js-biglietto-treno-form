66a928c54529df8e4690a79638fdf0249e99ae4e
danielebd <danielebalebalestro@gmail.com>
2023-02-22 17:21:23 +0100

    recupero dato non funziona

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 'use strict';
 
+const percorso = document.getElementById("km").value;
+const eta = document.getElementById("age").value;
+
+inputButton.addEventListener('click',
+    function () {
+        document.getElementById("km").value;
+    }
+)
+
+
+console.log('km da percorrere: ' + percorso);
+console.log('età passeggero: ' + eta);
