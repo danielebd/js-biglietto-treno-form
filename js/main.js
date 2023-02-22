95205dc6f113c19c9911e937f7a5b28036bb960d
danielebd <danielebalebalestro@gmail.com>
2023-02-22 17:38:16 +0100

    correzione estrazione dato da input "number"

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 'use strict';
 
-const percorso = document.getElementById("km").value;
+
 const eta = document.getElementById("age").value;
 
 inputButton.addEventListener('click',
     function () {
-        document.getElementById("km").value;
+        const percorso = document.getElementById("km").value;
+        document.getElementById("inputButton").innerHTML = percorso;
+        console.log('km da percorrere: ' + percorso);
     }
 )
 
 
-console.log('km da percorrere: ' + percorso);
+
 console.log('età passeggero: ' + eta);
