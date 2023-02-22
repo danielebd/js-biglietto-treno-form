5db602e1ce4150a8232e0c2924551a564d602a3b
danielebd <danielebalebalestro@gmail.com>
2023-02-22 21:28:48 +0100

    inserimento ultimi elementi su js

--- js/main.js
+++ js/main.js
@@ -18,18 +18,35 @@ inputButton.addEventListener('click',
             prezzo -= prezzo * 0.4;
             //prezzo = prezzo - prezzo * 0.4;
             console.log('prezzo scontato over 65 è di:' + prezzo + '€');
+            document.querySelector('#type').innerHTML = 'Biglietto ridotto 40%';
         }
         
         else if(eta == 1) {
             prezzo -= prezzo * 0.2;
             //prezzo = prezzo  - prezzo * 0.2;
             console.log('prezzo scontato per i minori 18 è di: ' + prezzo + '€');
+            document.querySelector('#type').innerHTML = 'Biglietto ridotto 20%';
+        }
+
+        else {
+            document.querySelector('#type').innerHTML = 'Biglietto Standard';
         }
         
         
         prezzo = prezzo.toFixed(2);
         console.log(prezzo + '€');
         
+        document.querySelector('#prezzo').innerHTML = prezzo + '€';
+
+        const name = document.getElementById("name").value;
+        console.log('nome passeggero: ' + eta);
+
+        document.querySelector('#person').innerHTML = name;
+
+        document.getElementById("carrozza").innerHTML = Math.floor(Math.random() * 10) + 1;
+
+        document.getElementById("cp").innerHTML = Math.floor(Math.random() * 99999) + 10000;
+
     }
 )
 
